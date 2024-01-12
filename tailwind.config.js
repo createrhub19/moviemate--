fad1ff16c588c85c1b3be73c6eb657840aa3052f
createrhub19 <prajjawal19kmr@gmail.com>
2024-01-12 16:18:13 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    content: [
+      "./src/**/*.{html,js}",
+      "./node_modules/flowbite/**/*.js"
+    ],
+    darkMode: 'class',
+    theme: {
+      extend: {},
+    
+    },
+    plugins: [require('flowbite/plugin')],
+  }
